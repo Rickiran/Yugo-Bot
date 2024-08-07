d2fe22de4b53b4eb088e1be8ac06bee43a2e705e
Rick-System <rorihuelainga95@gmail.com>
2024-08-06 19:56:10 -0500

    AssistantPythonV1

--- Assistant_pyttsx3_main.py
+++ Assistant_pyttsx3_main.py
@@ -6,6 +6,9 @@ import Instruction_open as io
 import webbrowser
 import os
 import pandas as pd
+import Instruction_Connection_DB_SAP_B1_Hana as iHana
+from Python_Project.Weather_Interface import weather
+###############################---HERE ARE DEFINED YUGO BOT FUNCTIONS---###########################################
 
 def Speak(text):
     rate=100
@@ -31,7 +34,7 @@ def callback(recognizer, audio):
 
         if "hola" in text or "hey yugobot":
             Speak("Hola señor, como puedo ayudarte hoy?")
-            recognize_main()
+            prompt()
 
     except sr.UnknownValueError:
         Speak("Oops No pude entenderte")
@@ -41,60 +44,97 @@ def start_recognizer():
     r.listen_in_background(source,callback)
     time.sleep(1000000)
 
-def recognize_main():
+def listen_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Say something!")
+        Speak("Estoy a la espera de sus comentarios")
         audio = r.listen(source)
-    data=""
     try:
-        data= r.recognize_google(audio)
-        print("You said:" + data.lower())
-
-        if "como estas hoy" in data:
-            Speak("me está yendo super bien y a ti?")
-        elif "modificar excel" in data:
-            Speak("Perfecto necesito que me brindes el nombre del archivo")
-            time.sleep(2)
-            rf=ie.name_file()
-            if rf:
-                Speak("Ahora necesito en que celda deseas que lo modifiques")
-                time.sleep(2)
-                cm = ie.cell_location()
-                Speak("Por ultimo necesito que me brindes el nuevo valor")
-                time.sleep(2)
-                nv = ie.new_valor()
-                ie.mod_excel(cm, nv, rf)
-            else:
-                Speak("Archivo no encontrado por ")
-        elif "abre youtube por favor" in data:
-            Speak("Okey procederé a abrir youtube")
-            webbrowser.open("http://www.youtube.com")
-
-        elif "abre google por favor" in data:
-            Speak("Okey procederé a abrir google")
-            webbrowser.open("http://www.google.com")
-
-        elif "abre notepad por favor" in data:
-            Speak("Okey procederé a abrir notepad")
-            os.system("notepad.exe")
-
-        elif "reproducir musica" in data:
-            Speak("Que musica le gustaría escuchar?")
-            io.play_music()
-
-        elif "buscar en google" in data:
-            query = data.split("buscar en google")[-1].strip()
-            Speak(f"Buscando {query} en Google")
-            webbrowser.open(f"https://www.google.com/search?q={query}")
-
-        else:
-            Speak("Perdon no pude entender su solicitud vuelva a intentarlo por favor")
-
+        data = r.recognize_google(audio)
+        return data
     except sr.UnknownValueError:
         print("Yugo bot no pudo entender su solicitud")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service {0}".format(e))
 
+def prompt():
+
+    prompt=listen_command()
+
+    if "como estas hoy" in prompt:
+            Speak("me está yendo super bien y a ti?")
+    elif "modificar excel" in prompt:
+        Speak("Perfecto necesito que me brindes el nombre del archivo")
+        time.sleep(2)
+        rf=ie.name_file()
+        if rf:
+            Speak("Ahora necesito en que celda deseas que lo modifiques")
+            time.sleep(2)
+            cm = ie.cell_location()
+            Speak("Por ultimo necesito que me brindes el nuevo valor")
+            time.sleep(2)
+            nv = ie.new_valor()
+            ie.mod_excel(cm, nv, rf)
+        else:
+            Speak("Archivo no encontrado por ")
+
+    elif "reporte socio" in prompt:
+        # Steps to consider making a connection to Hana
+        # Take into consideration that this variables must be called at main class and execute the function
+        host = ''  # Open config file located in C and take a look and fill as value in variable host
+        port = 30015  # Default HANA port but you can set another one checking out config file
+        user = 'throw an username'
+        password = 'throw a password'
+        database = 'stick a database up'
+        connect = iHana.connect_hana(host, port, user, password, database)
+        if connect:
+            Speak("Indicame el nombre del socio de negocios a considerar")
+            cn = listen_command()
+            iHana.create_business_part_report(connect, cn)
+        else:
+            Speak("Falló en conectarse a la base de datos.")
+
+
+    elif "reporte items" in prompt:
+        host = ''
+        port = 30015
+        user = ''
+        password = ''
+        database = ''
+        connect = iHana.connect_hana(host, port, user, password, database)
+        if connect:
+            Speak("Indicame el articulo a insertar por favor")
+            item_name = listen_command()
+            iHana.create_item_report(connect, item_name)
+        else:
+            Speak("Failed to connect to the database.")
+
+    elif "abre youtube por favor" in prompt:
+        Speak("Okey procederé a abrir youtube")
+        webbrowser.open("http://www.youtube.com")
+
+    elif "abre google por favor" in prompt:
+        Speak("Okey procederé a abrir google")
+        webbrowser.open("http://www.google.com")
+
+    elif "abre notepad por favor" in prompt:
+        Speak("Okey procederé a abrir notepad")
+        os.system("notepad.exe")
+
+    elif "reproducir musica" in prompt:
+        Speak("Que musica le gustaría escuchar?")
+        io.play_music()
+
+    elif "buscar en google" in prompt:
+        query = prompt.split("buscar en google")[-1].strip()
+        Speak(f"Buscando {query} en Google")
+        webbrowser.open(f"https://www.google.com/search?q={query}")
+
+    else:
+        Speak("Perdon no pude entender su solicitud vuelva a intentarlo por favor")
+
+
+
 while 1:
     start_recognizer()
+###############################--HERE WILL START YUGO BOT GUI---##################################################
--- Instruction_Connection_DB_SAP_B1_Hana.py
+++ Instruction_Connection_DB_SAP_B1_Hana.py
@@ -1,41 +1,78 @@
 import pyhdb
-#If I want an embedded db choose sqlite3 as one
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 import sqlite3
 def connect_hana(host, port, user, password, database):
-    make_connection_hana = pyhdb.connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        database=database
-    )
-    return make_connection_hana
+    try:
+        make_connection_hana = pyhdb.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+        return make_connection_hana
+    except Exception as e:
+        print("Fallo en conectar a la base de datos Hana",e)
+        return None
 
-# Steps to consider making a connection to Hana
-## Take into consideration that this variables must be called at main class and execute the function
-host = '' #Open config file located in C and take a look and fill as value in variable host
-port = 30015  # Default HANA port but you can set another one checking out config file
-user = 'throw an username'
-password = 'throw a password'
-database = 'stick a database up'
 
+def fetch_customer_details(connection, card_name):
+    cursor = connection.cursor()
+    query = f"SELECT CardCode, CardName FROM OCRD WHERE CardName like '%{card_name}%'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return result
+
+def fetch_item_detail(connection,item_code):
+    cursor=connection.cursor()
+    query=f"SELECT ItemCode FROM OITM WHERE ItemName like '%{item_code}%'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return result
+
+def create_business_part_report(connection, card_name):
+    card_details = fetch_customer_details(connection, card_name)
+    if card_details:
+        c = canvas.Canvas(f"{card_name}_report.pdf", pagesize=letter)
+        c.drawString(100, 750, "Business Partner Report")
+        c.drawString(100, 735, f"Card Name: {card_details[1]}")
+        c.drawString(100, 720, f"Card Code: {card_details[0]}")
+        c.save()
+        print("Reporte creado exitosamete para el business partner:", card_name)
+    else:
+        print("No se encontraron detalles:", card_name)
+
+def create_item_report(connection, item_name):
+    item_details = fetch_item_detail(connection, item_name)
+    if item_details:
+        c = canvas.Canvas(f"{item_name}_report.pdf", pagesize=letter)
+        c.drawString(100, 750, "Item Report")
+        c.drawString(100, 735, f"Item Code: {item_details[0]}")
+        c.save()
+        print("Reporte creado exitosamente el item:", item_name)
+    else:
+        print("No se encontraron detalles:", item_name)
+
+
+"""
 connection_hana = connect_hana(host, port, user, password, database)
 #we gonna user cursor to
 cursor_hana = connection_hana.cursor()
 cursor_hana.execute("SELECT * FROM @table")
 result_hana= cursor_hana.fetchall()
 for row in result_hana:
-    print(row)
-
+    print(row)"""
+"""
 # Using SQLite we got an embedded db
 sqlite_connection = sqlite3.connect('local.db')
 sqlite_cursor = sqlite_connection.cursor()
-sqlite_cursor.execute("""CREATE TABLE IF NOT EXISTS local_table (
+sqlite_cursor.execute(CREATE TABLE IF NOT EXISTS local_table (
                             column1 TYPE,
                             column2 TYPE,
                             column3 TYPE
-                        )""")
-
+                        ))"""
+"""
 # Insert data into SQLite
 sqlite_cursor.executemany("INSERT INTO local_table VALUES (?, ?, ?)", result_hana)
 sqlite_connection.commit()
@@ -43,7 +80,7 @@ sqlite_connection.commit()
 # Close SQLite connection
 sqlite_cursor.close()
 sqlite_connection.close()
-"""
+""""""
 for row in result_hana:
     print(row)
 connection_hana.close()"""
